e9d84eea12b1d1fae58db8398b5a04f706a89c27
Arpita19 <arpita.sng@gmail.com>
2014-08-14 22:30:58 -0700

    first commit

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+import java.*;
+import java.io.*;
+import java.util.*;
+
+public class Calculator
+{
+	public int add(int a, int b){
+		int c=a+b;
+		System.out.println("The result of the addition of the numbers is:" +c);
+		return c;
+	}
+	
+	public int subtract(int a, int b){}
+	
+	public float multiply(float a, float b)
+	{}
+	
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
